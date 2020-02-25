4ed69bdf95985e995ced8e19c79513cb3fb3246f
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-02-24 22:45:10 -0300

    atualizando informações no modal

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let modalQT = 1;
+
 const c = (el) => document.querySelector(el); // função
 const cs = (el) => document.querySelectorAll(el);
 
@@ -7,23 +9,40 @@ pizzaJson.map((item, index) => {
 
     pizzaItem.setAttribute('data-key', index);
 
+    //informações das pizzas
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;    
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
 
+    //modal
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
 
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQT = 1;
+        
         let pizzaModal = c('.pizzaWindowArea .pizzaInfo').cloneNode(true);
-
+        //informações no modal
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-        
 
+        //seleção do tamanho 
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+
+        //tamanhos das pizzas
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQT;
+        
+        //animação do modal
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
