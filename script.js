b7244d6fdcb430cb8be0f9261fa1c7c44c258789
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-02-25 21:29:47 -0300

    botão + - e tamanho configurados

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ pizzaJson.map((item, index) => {
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
         });
 
+        //quantidade do modal
         c('.pizzaInfo--qt').innerHTML = modalQT;
         
         //animação do modal
@@ -65,3 +66,22 @@ function closeModal() {
 cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
 })
+
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if(modalQT > 1){
+        modalQT--;
+        c('.pizzaInfo--qt').innerHTML = modalQT;
+    }
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQT++;
+    c('.pizzaInfo--qt').innerHTML = modalQT;
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+    size.addEventListener('click', (e) => {
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
